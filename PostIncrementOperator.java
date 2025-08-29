00e0d2602a009f65d401245fcb9fa92e02bd2311
Aakarsh Kumar <86540895+aakarshkumar@users.noreply.github.com>
2025-08-29 02:15:31 -0500

    Create PostIncrementOperator.java

---
+++ PostIncrementOperator.java
@@ -0,0 +1,10 @@
+public class PostIncrementOperator
+{
+    public static void main(String[] args)
+    {
+        int x = 5;
+        int y = x++;
+        System.out.print(y+" is the value of y, but ");
+        System.out.println(x+" is the value of x.");
+    }
+}
