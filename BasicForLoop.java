ea15faf4a47423f15246a25867673f7cb75f9ecd
Aakarsh Kumar <86540895+aakarshkumar@users.noreply.github.com>
2025-08-29 02:13:12 -0500

    Create BasicForLoop.java

---
+++ BasicForLoop.java
@@ -0,0 +1,14 @@
+public class BasicForLoop
+{
+    public static void main(String[] args)
+    {
+        for (int i = 1; i <= 100; i++)
+        {
+            System.out.println(i);
+            for (int j = 1; j <= 100; j++)
+                {
+                System.out.println(j);
+                }
+        }
+    }
+}
